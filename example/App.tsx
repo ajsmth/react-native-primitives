3e66cc0c182d290164e4a1a47ab79ed5158717b7
andy <40680668+ajsmth@users.noreply.github.com>
2021-09-14 21:58:53 -0700

    add example

---
+++ example/App.tsx
@@ -0,0 +1,107 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { create } from "react-native-primitives";
+
+const Heading = create(Text, {
+  base: {
+    fontFamily: "Helvetica",
+  },
+  variants: {
+    align: {
+      center: {
+        textAlign: 'center',
+      },
+      left: {
+        textAlign: 'left'
+      }
+    },
+    size: {
+      large: {
+        fontSize: 28,
+        lineHeight: 34,
+      },
+      medium: {
+        fontSize: 20,
+        lineHeight: 28,
+      },
+      small: {
+        fontSize: 18,
+        lineHeight: 22,
+      },
+    },
+    color: {
+      normal: {
+        color: "blue",
+      },
+      success: {
+        color: "green",
+      },
+      danger: {
+        color: "red",
+      },
+    },
+  },
+  selectors: {
+    // when device theme is 'light'...
+    light: {
+      color: {
+        // ...any `Heading` with `color="success"`...
+        success: {
+          // ...will have these styles applied
+          color: "orange",
+        },
+      },
+    },
+
+    // selectors can also target a11y traits
+    grayScale: {
+      color: {
+        success: {
+          color: "lightgray",
+        },
+      },
+    },
+
+    //  ...and screen width and height
+    width: {
+      // when `width > 768`...
+      "> 500": {
+        size: {
+          // ...update these styles
+          medium: {
+            fontSize: 38,
+            lineHeight: 36,
+          },
+        },
+      },
+    },
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Heading size="large" color="success">
+        Hi...try updating the theme
+      </Heading>
+
+      <View style={{ height: 12 }} />
+
+      <Heading size="medium" color="normal" align="center">
+        ...and rotating the phone
+      </Heading>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 14,
+  },
+});
